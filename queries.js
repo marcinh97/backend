d8a30ff0351601fa482326da68168f620dc6507f
marcinh97 <marcin.horak@gmail.com>
2019-04-08 16:41:20 +0200

    changed port

--- queries.js
+++ queries.js
@@ -1,5 +1,14 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//     user: 'ZpiAdmin@serverzpi',
+//     host: 'serverzpi.postgres.database.azure.com',
+//     database: 'savingapp',
+//     password: 'Zpi?kam1lNOWAK',
+//     port: 5432,
+//     ssl: true
+// });
+const pg = require('pg')
+var client = new pg.Client({
     user: 'ZpiAdmin@serverzpi',
     host: 'serverzpi.postgres.database.azure.com',
     database: 'savingapp',
@@ -8,8 +17,10 @@ const pool = new Pool({
     ssl: true
 });
 
+client.connect()
+
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM public."GeoLocations"', (error, results) => {
+    client.query('SELECT * FROM public."GeoLocations"', (error, results) => {
         if (error) {
             throw error
         }
